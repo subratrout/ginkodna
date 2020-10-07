b76b9275c97d21c38d28ba3cabebc2cb2fa5ffd8
Subrat Rout <subratnrout@gmail.com>
2020-10-07 13:39:26 -0500

    Added react-bootstrap and stylzed the form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import './App.css';
 
 function App() {
@@ -7,14 +10,25 @@ function App() {
       <header>
         <h1>GINKO-DNA</h1>
       </header>
-      <h2>Submit Your DNA Sequence header</h2>
-      <form>
-        <label>Name:<input type="text" name="name" /></label>
-        <label>Description:<input type="text" name="description" /></label>
-        <label>Sequence:<input type="textarea" name="sequence" /></label>
-        <br/>
-        <input className="button" type="submit" value="Submit" />
-      </form>
+      <div className="container">
+        <h2>Submit Your DNA Sequence header</h2>
+        <Form>
+          <Form.Group controlId="formName">
+            <Form.Label>Organism/Vector Name:</Form.Label>
+            <Form.Control type="text" placeholder="Enter organism/vector name" />
+            <Form.Text className="text-muted"></Form.Text>
+          </Form.Group>
+          <Form.Group controlId="formDescription">
+            <Form.Label>Description</Form.Label>
+            <Form.Control type="text" placeholder="Enter organism/vector description" />
+          </Form.Group>
+          <Form.Group controlId="formSequence">
+            <Form.Label>DNA Sequence</Form.Label>
+            <Form.Control as="textarea" rows = "3" placeholder="Enter organism/vector sequence" />
+          </Form.Group>
+          <Button className="btn-large" type="submit">Submit</Button>
+        </Form>
+      </div>     
     </div>
   );
 }
