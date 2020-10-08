f6c2fc44047b6c8c1f57fad617e58e3d2591806f
Subrat Rout <subratnrout@gmail.com>
2020-10-08 10:13:17 -0500

    Added initial form validations for name, description and regex for dna sequence match

--- src/Sequences.js
+++ src/Sequences.js
@@ -3,50 +3,62 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-
 class Sequences extends React.Component {
-  sequenceData;
 
   constructor(props){
     super(props);
-    this.state = {sequences: []};
-    this.onChangeName = this.onChangeName.bind(this);
-    this.onChangeDescription = this.onChangeDescription.bind(this);
-    this.onChangeSequence = this.onChangeSequence.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
+    this.state = {sequences:{name: '', description: '', sequence: ''}, errors: []};
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.name = React.createRef();
   }
-
-  onChangeName(e){
-    this.setState({name: e.target.value})
+  handleChange(sequence, e) {
+    let sequences = this.state.sequences;
+    sequences[sequence] = e.target.value;
+    this.setState({ sequences });
   }
 
-  onChangeDescription(e){
-    this.setState({description: e.target.value})
-  }
+  handleSubmit(e){
+    const seqPattern = new RegExp('^[ATCG]*$', 'i');
+    let errors = [];
 
-  onChangeSequence(e){
-    this.setState({sequence: e.target.value})
-  }
+    if (this.state.sequences.name.length < 3){
+      errors.push('Name must be atleast 3 characters long');
+    }
 
-  handleChange(){
-    this.setState({value: this.state.name})
-  }
+    if (this.state.sequences.description.length < 3) {
+      errors.push('Description must be atleast 3 characters long');
+    }
+    const validSequence = seqPattern.test(String(this.state.sequences.sequence).toUpperCase());
+    console.log(validSequence);
+    if(!validSequence){
+      errors.push('The sequence is not valid');
+    }
 
-  onSubmit(e){
-    alert('Your Sequence: ' + this.state.name + ' submitted.');
-    console.log(`Name: ${this.state.name}`);
-    e.preventDefault();
-    const single_seq = { name: this.state.name,
-                         description: this.state.description,
-                         sequence: this.state.sequence}
-    localStorage.setItem('sequences', JSON.stringify(single_seq))
     this.setState({
-      name: '',
-      description: '',
-      sequence: ''
-    })
+      errors: errors
+    });
+
+    if (errors.length > 0) {
+      return false;
+    } else {
+      alert("All form fields are valid. You can now submit the sequence!");
+    }
+    const sequenceData = JSON.parse(localStorage.getItem('sequences')) || [];
+    alert('Your Sequence: ' + this.state.sequences.name + ' submitted.');
+    console.log(`Name: ${this.state.sequences.name} description: ${this.state.sequences.description} sequence: ${this.state.sequences.sequence}`);
+
+    const single_seq = { sequenceName: this.state.sequences.name,
+                         sequenceDescription: this.state.sequences.description,
+                         sequence: this.state.sequences.sequence}
+    sequenceData.push(single_seq);
+    localStorage.setItem('sequences', JSON.stringify(sequenceData))
+
+    this.setState({sequences:{name: '', description: '', sequence: ''}});
+    e.preventDefault(); 
+  }
+
+  hasError(key) {
+    return this.state.errors.indexOf(key) !== -1;
   }
 
   componentDidMount(){
@@ -61,22 +73,30 @@ class Sequences extends React.Component {
         </header>
         <div className="container">
           <h2>Submit Your DNA Sequence header</h2>
-          <Form onSubmit = {this.onSubmit}>
-            <Form.Group controlId="formName">
-              <Form.Label>Organism/Vector Name:</Form.Label>
-              <Form.Control type="text" value={this.state.name} onChange={this.onChangeName} placeholder="Enter organism/vector name" />
-              <Form.Text className="text-muted"></Form.Text>
-            </Form.Group>
-            <Form.Group controlId="formDescription">
-              <Form.Label>Description</Form.Label>
-              <Form.Control type="text" value={this.state.description} onChange={this.onChangeDescription}  placeholder="Enter organism/vector description" />
-            </Form.Group>
-            <Form.Group controlId="formSequence">
-              <Form.Label>DNA Sequence</Form.Label>
-              <Form.Control as="textarea" value={this.state.sequence} rows="3" onChange={this.onChangeSequence} placeholder="Enter organism/vector sequence" />
-            </Form.Group>
-            <Button className="btn-large" type="submit">Submit</Button>
-          </Form>
+          <div class="col-md-6">
+            <Form noValidate onSubmit={this.handleSubmit}>
+              <Form.Group controlId="formName">
+                <Form.Label>Organism/Vector Name:</Form.Label>
+                <Form.Control type="text" value={this.state.sequences["name"] || ''} onChange={this.handleChange.bind(this, "name")} placeholder="Enter organism/vector name" />
+                <Form.Text className="text-muted"></Form.Text>
+                <Form.Control.Feedback type="invalid">
+                  className={
+                    this.hasError("firstname")
+                      ? "form-control is-invalid"
+                      : "form-control"}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="formDescription">
+                <Form.Label>Description</Form.Label>
+                <Form.Control type="text" value={this.state.sequences["description"] || ''} onChange={this.handleChange.bind(this, "description")} placeholder="Enter organism/vector description" />
+              </Form.Group>
+              <Form.Group controlId="formSequence">
+                <Form.Label>DNA Sequence</Form.Label>
+                <Form.Control as="textarea" value={this.state.sequences["sequence"] || ''} rows="3" onChange={this.handleChange.bind(this, "sequence")} placeholder="Enter organism/vector sequence" />
+              </Form.Group>
+              <Button className="btn-large" type="submit">Submit</Button>
+            </Form>
+          </div>
         </div>
       </div>
     );
